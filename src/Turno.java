1d5a031d48fc654587e2f49d2303ca5e59b442e7
Martin <40306986m@gmail.com>
2023-05-20 19:39:45 -0300

    actualizacion martin

--- src/Turno.java
+++ src/Turno.java
@@ -1,7 +1,56 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Turno {
     private Cliente cliente;
     private Factura factura;
     private LocalDate fecha;
+    public ArrayList <Turno> ListaTurnos;
+
+
+    public Turno(Cliente cliente, Factura factura, LocalDate fecha) {
+        this.cliente = cliente;
+        this.factura = factura;
+        this.fecha = fecha;
+        this.ListaTurnos=new ArrayList<>();
+    }
+    public void agregarListaTurno(Cliente cliente,Factura factura, LocalDate fecha)
+    {
+        Turno turno= new Turno(cliente,factura,fecha);
+        this.ListaTurnos.add(turno);
+    }
+    public Turno(){this.ListaTurnos=new ArrayList<>();}
+
+    @Override
+    public String toString() {
+        return "Turno{" +
+                "cliente=" + cliente +
+                ", factura=" + factura +
+                ", fecha=" + fecha +
+                ", ListaTurnos=" + ListaTurnos +
+                '}';
+    }
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Factura getFactura() {
+        return factura;
+    }
+
+    public void setFactura(Factura factura) {
+        this.factura = factura;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
 }

5720c519a2111e3f5072ba2b5fd04218c284c567
julianZ99 <128180017+julianZ99@users.noreply.github.com>
2023-06-03 11:52:20 -0300

    funciona mostrar clientes GUI desde arraylist

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class MainFrame {
     private JPanel jp;
@@ -11,22 +13,39 @@ public class MainFrame {
     private JButton VERCLIENTESButton;
     private JButton AGREGARCLIENTEButton;
     private JButton ELIMINARCLIENTEButton;
-    private JPanel verTurnos;
-    private JFormattedTextField formattedTextField1;
+    private JPanel verClientes;
+    private JList listadoClientes;
 
 
     public MainFrame() {
-        VERTURNOSButton.addActionListener(new ActionListener() {
+        VERCLIENTESButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JFrame frame = new JFrame("TURNOS");
-                frame.setContentPane(new MainFrame().verTurnos);
-                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                frame.pack();
-                frame.setSize(300, 200);
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
 
+                Cliente cliente1 = new Cliente("Julian", "Zapata", "2234567899");
+                Cliente cliente2 = new Cliente("Martin", "Montagner", "2234567899");
+                Cliente cliente3 = new Cliente("Franco", "Guidi", "2234567899");
+                Cliente cliente4 = new Cliente("Kevin", "Tolosa", "2234567899");
+
+
+                ArrayList<Cliente> clientes =new ArrayList<Cliente>();
+                clientes.add(cliente1);
+                clientes.add(cliente2);
+                clientes.add(cliente3);
+                clientes.add(cliente4);
+
+                String title = "CLIENTES";
+                JFrame f = new JFrame(title);
+                f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                JList list = new JList(clientes.toArray());
+                JScrollPane scrollPane = new JScrollPane(list);
+
+                Container contentPane = f.getContentPane();
+                contentPane.add(scrollPane, BorderLayout.CENTER);
+
+                f.setSize(300, 400);
+                f.setLocationRelativeTo(null);
+                f.setVisible(true);
 
             }
         });
